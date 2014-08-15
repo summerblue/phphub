22cc5524451dba880d4a1f16d01303ee2e915a0c
Charlie Jade <summer.alex07@gmail.com>
2014-08-15 16:12:40 +0800

    redesign of the wiki page

--- app/views/pages/wiki.blade.php
+++ app/views/pages/wiki.blade.php
@@ -2,18 +2,20 @@
 
 @section('content')
 
-<div class="col-md-6 col-md-offset-3">
+
+<div class="{{ count($topics) > 20 ?: 'col-md-6 col-md-offset-3' }} ">
+
   <div class="panel panel-default">
 
     <div class="panel-heading">
       <h3 class="panel-title text-center">社区 Wiki</h3>
     </div>
     
-    <div class="panel-body">
+    <div class="panel-body remove-padding-vertically remove-padding-horizontal">
 
       <div class="list-group">
         @foreach ($topics as $topic)
-          <a class="list-group-item" href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+          <a class="list-group-item {{ count($topics) <= 20 ?: 'col-md-6' }}" href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
             {{ str_limit($topic->title, '100') }}
           </a>
         @endforeach
@@ -22,6 +24,7 @@
     </div>
 
   </div>  
+
 </div>
 
 
