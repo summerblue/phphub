9b26bc294aa9d4a8e570b1a5e97e92cc37516471
Charlie Jade <summer.alex07@gmail.com>
2014-08-17 16:48:49 +0800

    add home message

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
+<div class="box text-center">
+  功能正在完善中，欢迎 <a href="https://github.com/summerblue/phphub">贡献代码</a> 或 <a href="https://github.com/summerblue/phphub/issues">提交 Issue</a>。
+</div>
+
 <div class="panel panel-default list-panel">
   <div class="panel-heading">
     <h3 class="panel-title text-center">社区精华帖</h3>
