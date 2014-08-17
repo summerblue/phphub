442391b572b544baf7d43da857329f5f47c5c94f
Charlie Jade <summer.alex07@gmail.com>
2014-08-17 14:21:20 +0800

    oauth key

--- app/config/packages/artdarek/oauth-4-laravel/config.php
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -18,8 +18,8 @@ return [
 	 */
 	'consumers' => [
         'GitHub' => [
-            'client_id'     => 'eefd4111fbcb0e1d0fb9',
-            'client_secret' => '3dce7078f20bc10a1f6bef559b81787648f1b372',
+            'client_id'     => getenv('client_id'),
+            'client_secret' => getenv('client_secret'),
             'scope'         => ['user'],
         ],
     ],
