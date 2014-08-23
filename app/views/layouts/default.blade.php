64a25e2dbb62fc9672576c9fa2ea405d5b5e8c85
Charlie Jade <summer.alex07@gmail.com>
2014-08-23 08:47:59 +0800

    hiding the stupid baidu analytics logo

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -104,10 +104,13 @@ PHPhub - PHP & Laravel的中文社区
 		  ga('send', 'pageview');
 		</script>
 		
-		<script type="text/javascript">
-		var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-		document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F9c9bedc0d968b38ed0ab8ce47d86c80e' type='text/javascript'%3E%3C/script%3E"));
-		</script>
+		<div class="hide">
+				
+			<script type="text/javascript">
+			var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+			document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F9c9bedc0d968b38ed0ab8ce47d86c80e' type='text/javascript'%3E%3C/script%3E"));
+			</script>
+		</div>
 
 
 	</body>
