6800346d29e7052b36e50c6ac0e5d61cb3ffe000
Charlie Jade <summer.alex07@gmail.com>
2014-08-23 20:54:29 +0800

    code styling

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -7,7 +7,7 @@ class PagesController extends \BaseController {
 	public function __construct(Topic $topic)
     {
     	parent::__construct();
-    	
+
         $this->topic = $topic;
     }
 
@@ -38,7 +38,7 @@ class PagesController extends \BaseController {
 		$topics = $this->topic->getWikiList();
 		return View::make('pages.wiki', compact('topics'));
 	}
-	
+
 	/**
 	 * 搜索功能
 	 */
@@ -47,33 +47,33 @@ class PagesController extends \BaseController {
 		$query = Security::clean(Input::get('q'));
 		return Redirect::away('https://www.google.com/search?q=site:phphub.org ' . $query, 301);
 	}
-	
+
 	/**
 	 * feed
 	 */
 	public function feed()
 	{
 		$topics = Topic::Excellent()->orderBy('created_at', 'desc')
-									->with('user')
-									->limit(20)
-									->get();
+                                    ->with('user')
+                                    ->limit(20)
+                                    ->get();
 		$channel =[
-    		'title' => 'PHPhub - PHP & Laravel的中文社区', 
-    		'description' => 'PHPhub是 PHP 和 Laravel 的中文社区，在这里我们讨论技术, 分享技术。', 
+            'title' => 'PHPhub - PHP & Laravel的中文社区',
+            'description' => 'PHPhub是 PHP 和 Laravel 的中文社区，在这里我们讨论技术, 分享技术。',
     		'link' => URL::route('feed')
     	];
 
 		$feed = Rss::feed('2.0', 'UTF-8');
-	    
+
 	    $feed->channel($channel);
 
 	    foreach ($topics as $topic)
 	    {
 	        $feed->item([
-		        		'title' => $topic->title, 
-		        		'description|cdata' => str_limit($topic->body, 200), 
-		        		'link' => URL::route('topics.show', $topic->id), 
-	        		]);
+                'title' => $topic->title,
+                'description|cdata' => str_limit($topic->body, 200),
+                'link' => URL::route('topics.show', $topic->id),
+                ]);
 	    }
 
 	    return Response::make($feed, 200, array('Content-Type' => 'text/xml'));
