2d9273858317da06907d39af94e39365db394ec1
Charlie Jade <summer.alex07@gmail.com>
2014-08-23 23:03:03 +0800

    remove fork me on github

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -112,6 +112,6 @@ PHPhub - PHP & Laravel的中文社区
 			</script>
 		</div>
 
-<style>#forkongithub a{background:#ddd;color:#000;text-decoration:none;font-family:arial,sans-serif;text-align:center;font-weight:bold;padding:5px 40px;font-size:1rem;line-height:2rem;position:relative;transition:0.5s;}#forkongithub a:hover{background:#c11;color:#fff;}#forkongithub a::before,#forkongithub a::after{content:"";width:100%;display:block;position:absolute;top:1px;left:0;height:1px;background:#fff;}#forkongithub a::after{bottom:1px;top:auto;}@media screen and (min-width:800px){#forkongithub{position:fixed;display:block;top:0;left:0;width:200px;overflow:hidden;height:200px;z-index:9999;}#forkongithub a{width:200px;position:absolute;top:40px;left:-46px;transform:rotate(-45deg);-webkit-transform:rotate(-45deg);-ms-transform:rotate(-45deg);-moz-transform:rotate(-45deg);-o-transform:rotate(-45deg);box-shadow:4px 4px 10px rgba(151, 151, 151, 0.8);}}</style><span id="forkongithub"><a href="https://github.com/summerblue/phphub" target="_blank">Fork me on GitHub</a></span>
+
 	</body>
 </html>
