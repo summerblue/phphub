24929a0a6a7de7a5ebebac2d6f6dbd8672e49094
Charlie Jade <summer.alex07@gmail.com>
2014-08-24 10:27:08 +0800

    convert about page markdown to html

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -1,79 +1,79 @@
 @extends('layouts.default')
 
 @section('title')
-关于我们_@parent 
+关于我们_@parent
 @stop
 
 @section('content')
-	
-<?php 
-	$body = <<<MARKDOWN
 
-# 关于 PHPHub
+    <div class="panel">
+    	<div class="panel-body">
 
-PHPHub 是积极向上的 PHP & Laravel 开发者社区. 
+        <div class="markdown-body" id="emojify">
+            <h1>关于 PHPHub</h1>
 
-在这里没有商业广告, 没有灌水, 没有千篇一律新手问答, 有的是关于新技术的讨论和分享. 
+            <p>PHPHub 是积极向上的 PHP &amp; Laravel 开发者社区. </p>
 
-本项目 [源代码在此 at Github](https://github.com/summerblue/phphub), 欢迎贡献代码, Feature Request 和 Bug Report [请到此发表](https://github.com/summerblue/phphub/issues).
+            <p>在这里没有商业广告, 没有灌水, 没有千篇一律新手问答, 有的是关于新技术的讨论和分享. </p>
 
+            <p>本项目 <a href="https://github.com/summerblue/phphub">源代码在此 at Github</a>, 欢迎贡献代码, Feature Request 和 Bug Report <a href="https://github.com/summerblue/phphub/issues">请到此发表</a>.</p>
 
-## 关于新手问题
+            <h2>关于新手问题</h2>
 
-这个社区不是用来问新手问题的, 如果你是新手, 请先看 http://www.phptherightway.com/ , 此文档能给你构建一个合理的 PHP 基本知识体系. 
+            <p>这个社区不是用来问新手问题的, 如果你是新手, 请先看 http://www.phptherightway.com/ , 此文档能给你构建一个合理的 PHP 基本知识体系. </p>
 
-第一次接触 Laravel? 这里是 [官方文档](http://laravel.com/docs/quick), 多看几遍, 你想知道的都在那里面. 
+            <p>第一次接触 Laravel? 这里是 <a href="http://laravel.com/docs/quick">官方文档</a>, 多看几遍, 你想知道的都在那里面. </p>
 
-在学习上遇到问题的时候, 请先 Google. 
+            <p>在学习上遇到问题的时候, 请先 Google. </p>
 
-如果觉得你的问题比较独占, 需要单独提问的话, 请到 http://segmentfault.com/t/php 发表你的问题, 谢谢. 
+            <p>如果觉得你的问题比较独占, 需要单独提问的话, 请到 http://segmentfault.com/t/php 发表你的问题, 谢谢. </p>
 
-## 远景 Vision 
+            <h2>远景 Vision</h2>
 
-### 在这里的我们
+            <h3>在这里的我们</h3>
 
-* 热爱开源, 尊重开源;
-* 热爱技术, 为最新最潮的技术狂热;
-* 热爱学习, 热爱互联网;
-* 热爱 PHP & Laravel;
+            <ul>
+            <li>热爱开源, 尊重开源;</li>
+            <li>热爱技术, 为最新最潮的技术狂热;</li>
+            <li>热爱学习, 热爱互联网;</li>
+            <li>热爱 PHP &amp; Laravel;</li>
+            </ul>
 
-### 在这里我们可以 
+            <h3>在这里我们可以</h3>
 
-* 分享生活见闻, 分享知识
-* 接触新技术
-* 为自己的创业项目找合伙人
-* 讨论技术解决方案
-* 自发线下聚会
-* 遇见志同道合的人
-* 发现更好工作机会
-* 甚至是开始另一个神奇的开源项目
-* ... 
+            <ul>
+            <li>分享生活见闻, 分享知识</li>
+            <li>接触新技术</li>
+            <li>为自己的创业项目找合伙人</li>
+            <li>讨论技术解决方案</li>
+            <li>自发线下聚会</li>
+            <li>遇见志同道合的人</li>
+            <li>发现更好工作机会</li>
+            <li>甚至是开始另一个神奇的开源项目</li>
+            <li>... </li>
+            </ul>
 
-### 在这里我们不可以 
+            <h3>在这里我们不可以</h3>
 
-* 这里绝对不讨论任何有关盗版软件、音乐、电影如何获得的问题 
-* 这里绝对不会全文转载任何文章，而只会以链接方式分享 
-* 这里感激和崇尚美的事物 
-* 这里尊重原创 
-* 这里反对中文互联网上的无信息量习惯如“顶”，“沙发”，“前排”，“留名”，“路过” 
+            <ul>
+            <li>这里绝对不讨论任何有关盗版软件、音乐、电影如何获得的问题 </li>
+            <li>这里绝对不会全文转载任何文章，而只会以链接方式分享 </li>
+            <li>这里感激和崇尚美的事物 </li>
+            <li>这里尊重原创 </li>
+            <li>这里反对中文互联网上的无信息量习惯如“顶”，“沙发”，“前排”，“留名”，“路过” </li>
+            </ul>
 
-## Laravel?
+            <h2>Laravel?</h2>
 
-为什么是 Laravel? 为什么不是 CI, Symfony, CakePHP, ThinkPHP ... 
+            <p>为什么是 Laravel? 为什么不是 CI, Symfony, CakePHP, ThinkPHP ... </p>
 
-Because Laravel is amazing and It is the future.
+            <p>Because Laravel is amazing and It is the future.</p>
 
-http://www.zhihu.com/question/19558755/answer/23062110 
+            <p>http://www.zhihu.com/question/19558755/answer/23062110 </p>
 
-MARKDOWN;
+        </div>
 
-?>
-
-	<div class="panel">
-
-		<div class="panel-body">
-			@include('topics.partials.body', array('body' => $body))
-		</div>
-	</div>
+    	</div>
+    </div>
 
 @stop
