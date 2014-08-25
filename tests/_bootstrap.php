e0915e71ed6b58491260bc1aee1ec1aa01a44bbd
Charlie Jade <summer.alex07@gmail.com>
2014-08-25 11:54:03 +0800

    fixing travis unable to load Eloquent

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,7 @@
 <?php
-// This is global bootstrap for autoloading 
+// This is global bootstrap for autoloading
+
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/start.php';
+
+$app->boot();
