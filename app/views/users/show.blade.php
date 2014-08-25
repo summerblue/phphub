f3c30ed412127848e7671f859eee80b7b9951a85
Charlie Jade <summer.alex07@gmail.com>
2014-08-25 09:35:16 +0800

    fixing the user introduction not show

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('title')
-{{{ $user->name }}} 的个人资料_@parent 
+{{{ $user->name }}} 的个人资料_@parent
 @stop
 
 @section('content')
@@ -13,16 +13,16 @@
   </div>
 
   <div class="main-col col-md-9 left-col">
-  
-    @if ($user->description)
-      <div class="box text-center">{{{ $user->description }}}</div>
+
+    @if ($user->introduction)
+      <div class="box text-center">{{{ $user->introduction }}}</div>
     @endif
 
     <div class="panel panel-default">
       @include('users.partials.infonav', ['current' => 'basicinfo'])
 
-      <iframe src="http://lab.lepture.com/github-cards/card.html?user={{ $user->name }}&target=blank" frameborder="0" scrolling="0" width="100%" height="146px" allowtransparency></iframe> 
-    </div>  
+      <iframe src="http://lab.lepture.com/github-cards/card.html?user={{ $user->name }}&target=blank" frameborder="0" scrolling="0" width="100%" height="146px" allowtransparency></iframe>
+    </div>
 
     <div class="panel panel-default">
 
@@ -30,11 +30,11 @@
         <li class="active"><a href="#recent_replies" role="tab" data-toggle="tab">最新评论</a></li>
         <li><a href="#recent_topics" role="tab" data-toggle="tab">最近话题</a></li>
       </ul>
-      
+
       <div class="panel-body remove-padding-vertically remove-padding-horizontal">
         <!-- Tab panes -->
         <div class="tab-content">
-        
+
           <div class="tab-pane active" id="recent_replies">
 
             @if (count($replies))
@@ -42,7 +42,7 @@
             @else
               <div class="empty-block">还未留下任何评论~~</div>
             @endif
-            
+
           </div>
 
           <div class="tab-pane" id="recent_topics">
