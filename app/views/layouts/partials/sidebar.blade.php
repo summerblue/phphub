b8cb0b964b6e32ee48e563896661557286a499db
Charlie Jade <summer.alex07@gmail.com>
2014-09-01 22:19:32 +0800

    little modification of the sidebar

--- app/views/layouts/partials/sidebar.blade.php
+++ app/views/layouts/partials/sidebar.blade.php
@@ -27,8 +27,8 @@
           </div>
           <div class="panel-body">
             <ul class="list">
+                <li><a href="http://laravel-china.org/">Laravel 4.2 文档</a></li>
                 <li><a href="http://wulijun.github.io/php-the-right-way/">PHP The Right Way 中文版</a></li>
-                <li><a href="http://laravel-china.org/">Laravel4.2 文档</a></li>
                 <li><a href="https://github.com/PizzaLiu/PHP-FIG">PHP-FIG 编程规范中文</a></li>
                 <li><a href="http://www.phpcomposer.com/">Composer 中文文档</a></li>
             </ul>
