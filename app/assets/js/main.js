2e66cd293569b8899c51b377bd8ca8d130099909
Paul King <freedomlijinfa@gmail.com>
2014-09-24 19:52:04 +0800

    Add auto complete @user function

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -30,6 +30,7 @@
             self.initExternalLink();
             self.initTimeAgo();
             self.initEmoji();
+            self.initAutoAtUser();
             self.initScrollToTop();
             self.initTextareaAutoResize();
             self.initHeightLight();
@@ -102,6 +103,38 @@
                     maxCount: 5
                 }
             ]);
+        },
+
+        /**
+         * Autocomplete @user
+         */
+        initAutoAtUser: function() {
+            var at_users = [],
+                  user;
+            $users = $('.media-heading').find('a');
+            for (var i = 0; i < $users.length; i++) {
+                user = $users.eq(i).html();
+                if ($.inArray(user, at_users) == -1) {
+                    at_users.push(user);
+                };
+            };
+
+            $('textarea').textcomplete([{
+                mentions: at_users,
+                match: /\B@(\w*)$/,
+                search: function(term, callback) {
+                    callback($.map(this.mentions, function(mention) {
+                        return mention.indexOf(term) === 0 ? mention : null;
+                    }));
+                },
+                index: 1,
+                replace: function(mention) {
+                    return '@' + mention + ' ';
+                }
+            }], {
+                appendTo: 'body'
+            });
+
         },
 
         /**
