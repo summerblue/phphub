032f008da370e42d4545b3833e445db8945571e6
NauxLiu <nauxliu@gmail.com>
2015-03-28 15:39:10 +0800

    Fix user avatar show error, In local environment.

--- app/Phphub/Presenters/UserPresenter.php
+++ app/Phphub/Presenters/UserPresenter.php
@@ -2,15 +2,24 @@
 
 use Laracasts\Presenter\Presenter;
 use Route;
+use Config;
 
 class UserPresenter extends Presenter
 {
     /**
-     * Present a link to the user gravatar
+     * Present a link to the user gravatar.
      */
     public function gravatar($size = 80)
     {
-        return cdn('uploads/avatars/' . $this->avatar) . "?imageView2/1/w/{$size}/h/{$size}";
+        if (Config::get('app.url_static')) {
+            //Using Qiniu image processing service.
+            return cdn('uploads/avatars/'.$this->avatar)."?imageView2/1/w/{$size}/h/{$size}";
+        }
+
+        $github_id = $this->github_id;
+        $domainNumber = rand(0, 3);
+
+        return "https://avatars{$domainNumber}.githubusercontent.com/u/{$github_id}?v=2&s={$size}";
     }
 
     public function userinfoNavActive($anchor)
