2583dd2ea53defd20f5466bf204121f3fcbf23b0
Charlie Jade <summer.alex07@gmail.com>
2015-09-26 09:50:18 +0800

    add user badge to presentor

--- app/Phphub/Presenters/UserPresenter.php
+++ app/Phphub/Presenters/UserPresenter.php
@@ -3,6 +3,8 @@
 use Laracasts\Presenter\Presenter;
 use Route;
 use Config;
+use User;
+use Role;
 
 class UserPresenter extends Presenter
 {
@@ -25,5 +27,35 @@ class UserPresenter extends Presenter
     public function userinfoNavActive($anchor)
     {
         return Route::currentRouteName() == $anchor ? 'active' : '';
+    }
+
+    public function hasBadge()
+    {
+        $relations = Role::relationArrayWithCache();
+        $user_ids = array_pluck($relations, 'user_id');
+        return in_array($this->id, $user_ids);
+    }
+
+    public function badgeName()
+    {
+        $relations = Role::relationArrayWithCache();
+        $relation = array_first($relations, function($key, $value)
+        {
+            return $value->user_id == $this->id;
+        });
+
+        if (!$relation) {
+            return;
+        }
+
+        $roles = Role::rolesArrayWithCache();
+
+        $role = array_first($roles, function($key, $value) use( &$relation)
+        {
+            return $value->id == $relation->role_id;
+        });
+
+
+        return $role->name;
     }
 }
--- app/models/Role.php
+++ app/models/Role.php
@@ -4,4 +4,19 @@ use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
 {
+    public static function relationArrayWithCache()
+    {
+        return Cache::remember('all_assigned_roles', $minutes = 60, function()
+        {
+            return DB::table('assigned_roles')->get();
+        });
+    }
+
+    public static function rolesArrayWithCache()
+    {
+        return Cache::remember('all_roles', $minutes = 60, function()
+        {
+            return DB::table('roles')->get();
+        });
+    }
 }
