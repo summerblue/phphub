723999623fb912dfdab691166c565ef8a3cf4d00
NauxLiu <nauxliu@gmail.com>
2015-10-23 16:09:43 +0800

    typo

--- app/views/users/partials/login_QR.blade.php
+++ app/views/users/partials/login_QR.blade.php
@@ -1,4 +1,4 @@
 <div style="text-align: center; margin: 20px 0 15px">
-  <p style="margin-bottom: 10px">客户端登陆二维码</p>
+  <p style="margin-bottom: 10px">客户端登录二维码</p>
   <img style="height: 180px; width=180px;" " src="data:image/png;base64,{{ base64_encode($user->present()->loginQR(180)) }}">
 </div>
