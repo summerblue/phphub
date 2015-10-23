112e61f1030f8e85dacd683d4cd85f3a5d01e203
NauxLiu <nauxliu@gmail.com>
2015-10-23 14:11:59 +0800

    improve QR style

--- app/views/users/partials/login_QR.blade.php
+++ app/views/users/partials/login_QR.blade.php
@@ -1,4 +1,4 @@
-<div style="text-align: center; margin-top: 20px">
+<div style="text-align: center; margin: 20px 0 15px">
   <p style="margin-bottom: 10px">客户端登陆二维码</p>
   <img style="height: 180px; width=180px;" " src="data:image/png;base64,{{ base64_encode($user->present()->loginQR(180)) }}">
 </div>
