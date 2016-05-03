25b6bc68a39f57a65f0c93aee569436ca9504dab
Charlie Jade <cj@estgroupe.com>
2016-05-03 15:21:53 +0800

    fix laravel4.2 on homestead 0.3.3 - Invalid default value for 'created_at'

--- app/config/database.php
+++ app/config/database.php
@@ -61,6 +61,7 @@ return array(
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
+            'strict'    => true
         ),
 
         'pgsql' => array(
