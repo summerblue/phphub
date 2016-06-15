84f642e1510a8a6a40d99607615993be6db16a97
Charlie Jade <cj@estgroupe.com>
2016-06-15 15:46:39 +0800

    remove slack

--- app/Phphub/Core/Robot.php
+++ app/Phphub/Core/Robot.php
@@ -7,29 +7,6 @@ class Robot
 {
     public static function notify($content, $type, $topic, $user)
     {
-        if (!getenv('slack_endpoint') || App::environment() != 'production')
-        {
-            return;
-        }
-
-        $topic_link = '<' . route('topics.show', $topic->id) .'|'. $topic->title .'>';
-        $user_link = '<' . route('users.show', $user->id) .'|'. $user->name .'>';
-
-        switch ($type) {
-            case 'Reply':
-                    $message = 'New Reply Created, for Topic: '. $topic_link .',  by User: '. $user_link;
-                break;
-
-            case 'Topic':
-                    $message = 'New Topic Created, Title: '. $topic_link .',  by User: '. $user_link;
-                break;
-        }
-
-        Slack::attach([
-            'fallback'  => $message,
-            'text'      => $content,
-            'color'     => '#F0C49C',
-            'mrkdwn_in' => ['text']
-        ])->send($message);
+        return;
     }
 }
